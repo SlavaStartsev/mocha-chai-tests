b2278c9bf749747159487e5dd492c4872bc3386d
Slava Startsev <slava.startsev@tuta.io>
2017-04-05 01:15:37 +0300

    basicTest modified

--- src/test/basicTest.test.js
+++ src/test/basicTest.test.js
@@ -5,4 +5,8 @@ describe('basic test', () => {
   it('should be ok', () => {
     expect(false).to.not.equal.true;
   });
+
+  it('should be an error', () => {
+    expect(new Error).to.be.an('error');
+  });
 });
